05f49e0013693ef5038acf676e3e3903b12d4e08
Philippe Coval <rzr@gna.org>
2012-01-16 21:33:55 +0100

    Add: dh_make

---
+++ debian/acpi-call.default.ex
@@ -0,0 +1,10 @@
+# Defaults for acpi-call initscript
+# sourced by /etc/init.d/acpi-call
+# installed at /etc/default/acpi-call by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(acpi-call):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="acpi-call" command="/usr/bin/acpi-call"
